bb71d265f315d240cb85cc696852120f30e89125
Lukas Batema <lukasbatema@gmail.com>
2021-01-26 11:27:03 -0500

    Update index.py

--- index.py
+++ index.py
@@ -1,1 +1,10 @@
+# Tag Variables.
+tagInfo=''
 
+# <html>...</html> element.
+openHtml='<html>'
+closedHtml='</html>'
+def openHtmlFunction():
+  print('<!DOCTYPE html>')
+  print('  ' + openHtml)
+  return
